a2f72252550c823e10d6d756688e5f4e17aab59f
Nareik2610 <Nareik.2610@gmx.de>
2025-02-22 13:50:27 +0100

    update

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -6,5 +6,28 @@ func QuickSort(list []int) {
 		return
 	}
 
-	// TODO
+	// HINWEIS:
+	// Wählen Sie das erste Element der Liste als Pivot-Element.
+	// Erstellen Sie dann zwei Listen:
+	// - eine Liste mit allen Elementen, die kleiner als das Pivot-Element sind
+	// - eine Liste mit allen Elementen, die größer oder gleich dem Pivot-Element sind
+	// Rufen Sie QuickSort rekursiv für beide Listen auf.
+	// Fügen Sie die sortierten Listen zusammen, um die sortierte Liste zu erhalten.
+
+	pivot := list[0]
+	left := []int{}
+	right := []int{}
+
+	for _, v := range list[1:] {
+		if v < pivot {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+
+	QuickSort(left)
+	QuickSort(right)
+
+	copy(list, append(append(left, pivot), right...))
 }
